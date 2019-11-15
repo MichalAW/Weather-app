fb25ffb7da36ad5f3f0db413362fa57a522e0fbc
Michał Wanta <michal.wanta@euco.pl>
2019-11-15 14:02:11 +0100

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MichalAW/Book-searche/precache-manifest.4409276b3d7cc4c91a7e82b905edd099.js"
+  "/MichalAW/Weather-app/precache-manifest.f34caa1eb606f45c675d99c7b5153f28.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MichalAW/Book-searche/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MichalAW/Weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
